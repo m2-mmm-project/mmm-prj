94c7f9c89d347c0c49fa4d3a101df0c52098b208
maxleiko <leiko@braindead.fr>
2013-02-03 16:11:45 +0100

    improv UI agenda

--- ade-agenda/src/fr/istic/mmm/adeagenda/AgendaActivity.java
+++ ade-agenda/src/fr/istic/mmm/adeagenda/AgendaActivity.java
@@ -21,14 +21,15 @@ public class AgendaActivity extends Activity {
 
 	private static final String TAG = AgendaActivity.class.getSimpleName();
 	private static final String ITEM_NAME = "name";
+	private static final String ITEM_DATE = "date";
 	private static final String ITEM_START = "start";
 	private static final String ITEM_END = "end";
 	
 	private ListView eventList;
 	private SimpleAdapter eventAdapter;
 	private List<HashMap<String, String>> data;
-	private String from[] = {ITEM_NAME, ITEM_START, ITEM_END};
-	private int to[] = {R.id.event_name, R.id.event_start, R.id.event_end};
+	private String from[] = {ITEM_NAME, ITEM_DATE, ITEM_START, ITEM_END};
+	private int to[] = {R.id.event_name, R.id.event_date, R.id.event_start, R.id.event_end};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -58,14 +59,15 @@ public class AgendaActivity extends Activity {
 			public void run() {
 				CalendarReader reader = new CalendarReader(is);
 				List<Event> events = reader.allEvents();
-				
-				DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM);
+				DateFormat dfTime = DateFormat.getTimeInstance(DateFormat.SHORT);
+				DateFormat dfDate = DateFormat.getDateInstance(DateFormat.SHORT);
 				
 				for (Event e : events) {
 					final HashMap<String, String> item = new HashMap<String, String>();
 					item.put(ITEM_NAME, e.getName());
-					item.put(ITEM_START, df.format(e.getStart()));
-					item.put(ITEM_END, df.format(e.getEnd()));
+					item.put(ITEM_DATE, dfDate.format(e.getStart()));
+					item.put(ITEM_START, dfTime.format(e.getStart()));
+					item.put(ITEM_END, dfTime.format(e.getEnd()));
 					runOnUiThread(new Runnable() {
 						@Override
 						public void run() {
